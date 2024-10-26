14bef69fa375ce6c98663988d881a64d98d5a90d
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2024-10-27 00:44:07 +0500

    Refactored the Restaurant Food Card

--- lib/restaurant/view/restaurant_page.dart
+++ lib/restaurant/view/restaurant_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_store/components/Card/x_card.dart';
+import 'package:food_store/restaurant/components/restaurant_food_card.dart';
 import 'package:food_store/restaurant/model/restaurant.dart';
 import 'package:food_store/routes/route_transition.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -36,97 +37,7 @@ class RestaurantPage extends StatelessWidget {
               itemCount: restaurant.foods.length,
               itemBuilder: (context, index) {
                 final food = restaurant.foods[index];
-                return Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.r),
-                    border: Border.all(
-                      color: Colors.grey.shade400,
-                      width: 0.1.w,
-                    ),
-                    gradient: const LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.centerLeft,
-                      colors: [
-                        Color(0xFF1d102d),
-                        Colors.transparent,
-                      ],
-                    ),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 10.w,
-                      vertical: 10.h,
-                    ),
-                    width: 500.w,
-                    height: 110.h,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          // width: 200.w,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                food.name,
-                                style: TextStyle(
-                                  fontSize: 17.sp,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              vGap(3),
-                              Text(
-                                food.description,
-                                style: TextStyle(
-                                  fontSize: 12.sp,
-                                  fontWeight: FontWeight.w100,
-                                ),
-                              ),
-                              vGap(3),
-                              Text(
-                                '\$${food.price}',
-                                style: TextStyle(
-                                  fontSize: 15.sp,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Stack(
-                          children: [
-                            SizedBox(
-                              // color: Colors.amber,
-                              width: 120.w,
-                              height: 100.h,
-                              child: Image.asset(
-                                food.imgUrl,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            const Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: XCard(
-                                backgroundColor: Colors.black54,
-                                child: Icon(EvaIcons.plus),
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-                return XCard(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Menu Item $index'),
-                    subtitle: Text('Description of Menu Item $index'),
-                    trailing: Text('\$${index * 10}'),
-                  ),
-                );
+                return RestaurantFoodCard(food: food);
               },
               separatorBuilder: (context, index) {
                 return vGap(10);
